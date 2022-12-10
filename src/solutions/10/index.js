5088888d6733ef8e512c9100aa9549871f345272
Tom Dawes <tomdawes91@gmail.com>
2022-12-10 09:20:29 +0000

    Day 10

--- src/solutions/10/index.js
+++ src/solutions/10/index.js
@@ -7,16 +7,108 @@ const readFile = promisify(fs.readFile);
 
 const readInput = async () => readFile(path.join(__dirname, "input"), "utf-8");
 
-const parse = (input) => {};
+const parse = (input) => {
+  const lines = input.split("\n");
+  return lines.map((line) => {
+    const parts = line.split(" ");
+    switch (parts[0]) {
+      case "noop":
+        return { command: "noop", wait: 0 };
+      case "addx":
+        return { command: "addx", value: parseInt(parts[1], 10), wait: 1 };
+    }
+  });
+};
 
 const part1 = async () => {
   const input = await readInput();
-  const parsed = parse(input);
+  const commands = parse(input);
+
+  let cycle = 0;
+  let commandIndex = 0;
+
+  const scores = [];
+  const importantCycles = [20, 60, 100, 140, 180, 220];
+  const memory = { x: 1 };
+
+  const processCommand = (command) => {
+    if (command.command === "noop") {
+      // Do nothing
+    } else if (command.command === "addx") {
+      memory.x += command.value;
+    }
+  };
+
+  while (true) {
+    cycle += 1;
+
+    if (importantCycles.includes(cycle)) {
+      scores.push(cycle * memory.x);
+    }
+
+    const command = commands[commandIndex];
+    if (command.wait > 0) {
+      command.wait -= 1;
+    } else {
+      processCommand(command);
+      commandIndex += 1;
+    }
+
+    if (cycle === Math.max(...importantCycles)) {
+      break;
+    }
+  }
+
+  return _.sum(scores);
 };
 
 const part2 = async () => {
   const input = await readInput();
-  const parsed = parse(input);
+  const commands = parse(input);
+
+  let cycle = 0;
+  let commandIndex = 0;
+
+  const display = [];
+  let line = [];
+  const memory = { x: 1 };
+
+  const processCommand = (command) => {
+    if (command.command === "noop") {
+      // Do nothing
+    } else if (command.command === "addx") {
+      memory.x += command.value;
+    }
+  };
+
+  while (true) {
+    cycle += 1;
+
+    if (Math.abs(memory.x - ((cycle - 1) % 40)) <= 1) {
+      line.push("#");
+    } else {
+      line.push(".");
+    }
+
+    if (cycle % 40 === 0) {
+      display.push(line.join(""));
+      line = [];
+    }
+
+    const command = commands[commandIndex];
+    if (command.wait > 0) {
+      command.wait -= 1;
+    } else {
+      processCommand(command);
+      commandIndex += 1;
+    }
+
+    if (cycle === 240) {
+      break;
+    }
+  }
+
+  return display.join("\n");
 };
 
 part1().then(console.log).then(part2).then(console.log);
